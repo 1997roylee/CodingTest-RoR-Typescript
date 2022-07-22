2e1998dfa5511c27bc4193a88f6061b53adacef6
1997roylee <roy.lee@code4we.com>
2022-07-22 11:03:27 +0000

    add new route create todo item add new route bulk edit todo items change update todo item route path "todo" to "todo_item/:todo_item_id" and method "post" to "patch"

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   root to: "home#landing"
-  post "todo", to: "home#edit_todo_item"
-  post "reset", to: "home#reset_todo_item"
+  patch "todo_item/:todo_item_id", to: "home#edit_todo_item"
+  post "todo_item", to: "home#create_todo_item"
+  post "reset", to: "home#reset_todo_items"
+  post "bulk_edit", to: "home#bulk_edit_todo_items"
 end
