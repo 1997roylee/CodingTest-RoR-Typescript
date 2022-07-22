50de33b8f5918ae9bebc6206162b048156e6fdfc
1997roylee <roy.lee@code4we.com>
2022-07-22 11:00:39 +0000

    Refactor Todo List: 1) add display error/success message 2) factor checkbox onchange method 3) add checkbox for select all or reset all 4) refactor reset button

--- app/javascript/components/TodoList/index.tsx
+++ app/javascript/components/TodoList/index.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
-import { Container, ListGroup, Form } from "react-bootstrap";
+import { Container, ListGroup, Form, Alert } from "react-bootstrap";
 import { ResetButton } from "./uiComponent";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import { CreateTodo } from "../CreateTodo";
 
 type TodoItem = {
   id: number;
@@ -13,7 +14,10 @@ type Props = {
   todoItems: TodoItem[];
 };
 
-const TodoList: React.FC<Props> = ({ todoItems }) => {
+const TodoList: React.FC<Props> = (props) => {
+  const [message, setMessage] = React.useState<null | string>(null)
+  const [todoItems, setTodoItems] = React.useState<TodoItem[]>(props.todoItems)
+
   useEffect(() => {
     const token = document.querySelector(
       "[name=csrf-token]"
@@ -21,23 +25,67 @@ const TodoList: React.FC<Props> = ({ todoItems }) => {
     axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
   }, []);
 
-  const checkBoxOnCheck = (
+  const checkBoxOnCheck = async (
     e: React.ChangeEvent<HTMLInputElement>,
     todoItemId: number
-  ): void => {
-    axios.post("/todo", {
-      id: todoItemId,
-      checked: e.target.checked,
+  ): Promise<void> => {
+    try {
+      const response = await axios.patch(`/todo_item/${todoItemId}`, {
+        checked: e.target.checked,
+      });
+      if (response.status === 200) {
+        const index = todoItems.findIndex((todoItem: TodoItem) => todoItem.id === todoItemId)
+        todoItems[index].checked = !todoItems[index].checked
+        setTodoItems([...todoItems])
+        return;
+      }
+      setMessage("unknown error")
+    } catch {
+      setMessage("error")
+    }
+  };
+
+  const handleCheck = (todoItemId: number) => {
+    return (e: React.ChangeEvent<HTMLInputElement>) => checkBoxOnCheck(e, todoItemId)
+  }
+
+  const switchAllItemsCheckedStatus = React.useCallback((checked: boolean) => {
+    todoItems.forEach((todoItem: TodoItem) => todoItem.checked = checked)
+    setTodoItems([...todoItems])
+  }, [todoItems])
+
+  const resetButtonOnClick = (): void => {
+    axios.post("/reset").then(() => {
+      setMessage("Reset Successfully")
+      switchAllItemsCheckedStatus(false)
+
+    }).catch(() => {
+      setMessage("Cannot reset!")
     });
   };
 
-  const resetButtonOnClick = (): void => {
-    axios.post("/reset").then(() => location.reload());
-  };
+  const handleCheckAll = (e: React.ChangeEvent<HTMLInputElement>) => {
+    axios.post("/bulk_edit", {
+      checked: e.target.checked
+    }).then(() => {
+      switchAllItemsCheckedStatus(e.target.checked)
+    }).catch(() => {
+      setMessage("Cannot check all!")
+    })
+  }
+
+  const handleCreateSuccess = (response: AxiosResponse<any, any>) => {
+    todoItems.push(response.data.todo_item)
+    setTodoItems([...todoItems])
+  }
 
   return (
     <Container>
       <h3>2022 Wish List</h3>
+      {
+        message ? <Alert>{message}</Alert> : null
+      }
+      <CreateTodo onSuccess={handleCreateSuccess} />
       <ListGroup>
         {todoItems.map((todo) => (
           <ListGroup.Item key={todo.id}>
@@ -45,10 +93,15 @@ const TodoList: React.FC<Props> = ({ todoItems }) => {
               type="checkbox"
               label={todo.title}
               checked={todo.checked}
-              onChange={(e) => checkBoxOnCheck(e, todo.id)}
+              onChange={handleCheck(todo.id)}
             />
           </ListGroup.Item>
         ))}
+        <Form.Check
+          type="checkbox"
+          label="check all"
+          onChange={handleCheckAll}
+        />
         <ResetButton onClick={resetButtonOnClick}>Reset</ResetButton>
       </ListGroup>
     </Container>
