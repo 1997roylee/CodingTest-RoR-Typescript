3b5f52f07e5d7bc235bcfe80c358e52be7ab0edc
1997roylee <roy.lee@code4we.com>
2022-07-27 14:46:42 +0000

    add helper methods on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  helper_method :broadcast
+  helper_method :current_user_id
+
+  protected
+
+  def current_user_id
+    request.headers["X-USER-ID"]
+  end
+
+  def broadcast(type:, payload:, user_id:)
+    ActionCable.server.broadcast("todo_messages", {
+      type: type,
+      payload: payload,
+      user_id: user_id,
+    })
+  end
 end
