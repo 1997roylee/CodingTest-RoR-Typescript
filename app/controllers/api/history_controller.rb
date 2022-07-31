99477651e42047f1088ca12d9c1af0d0d268e962
1997roylee <roy.lee@code4we.com>
2022-07-31 07:17:38 +0000

    add render helper

--- app/controllers/api/history_controller.rb
+++ app/controllers/api/history_controller.rb
@@ -7,15 +7,21 @@ module Api
     ## get /api/history.json
     def index
       render(json: {
-               history: Version.order(id: :desc).map { |version| VersionSerializer.new(version).as_json },
-             })
+        history: Version.order(id: :desc).map { |version| VersionSerializer.new(version).as_json },
+      })
     end
 
     ## post /api/history/:version_id/revert
     ## post /api/history/:version_id/revert.json
     def revert
       @version.revert!
-      render(json: {})
+      todo_groups = TodoGroup.includes(:todos).all.order("todos.id").map do |todo_group|
+        ::TodoGroupSerializer.new(todo_group).as_json
+      end
+      broadcast(type: "Reload", payload: nil, user_id: current_user_id)
+      render_ok({})
+    rescue ActiveRecord::RecordNotUnique
+      render_errors(nil)
     end
 
     private
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include RenderHelper
   helper_method :broadcast
   helper_method :current_user_id
 
